0d48949ad949d6166690270dce20a28ee4f5559c
SwimmingTiger <hu60.cn@gmail.com>
2023-01-14 22:06:38 +0800

    refac: 移除pdnsd

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -17,20 +17,6 @@ LOCAL_PATH := $(call my-dir)
 ROOT_PATH := $(LOCAL_PATH)
 
 ########################################################
-## pdnsd library
-########################################################
-
-include $(CLEAR_VARS)
-
-PDNSD_SOURCES  := $(wildcard $(LOCAL_PATH)/pdnsd/src/*.c)
-
-LOCAL_MODULE    := pdnsd
-LOCAL_SRC_FILES := $(PDNSD_SOURCES:$(LOCAL_PATH)/%=%)
-LOCAL_CFLAGS    := -Wall -O2 -I$(LOCAL_PATH)/pdnsd -DHAVE_STPCPY
-
-include $(BUILD_EXECUTABLE)
-
-########################################################
 ## libancillary
 ########################################################
 
